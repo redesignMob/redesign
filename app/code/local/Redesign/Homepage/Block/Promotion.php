04824bb7339ada30f9043c05d55e192c5cf1c17e
alex loghin <alexloghin2002@yahoo.com>
2012-01-22 16:00:26 +0200

    Dinamycally load promotions

--- app/code/local/Redesign/Homepage/Block/Promotion.php
+++ app/code/local/Redesign/Homepage/Block/Promotion.php
@@ -1,6 +1,14 @@
 <?php
 class Redesign_Homepage_Block_Promotion extends Mage_Core_Block_Template
 {
-    
+    public function getPromotionList() {
+        $promotions = Mage::getModel('catalog/product')->getCollection()
+                            ->addAttributeToFilter('homepage_promotion',1)
+                            ->addAttributeToSort('name', 'ASC')
+                            ->setPageSize(3)
+                            ->load();
+       
+        return $promotions;
+    }
 }
 ?>
